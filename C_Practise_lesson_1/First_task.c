293e46fabe44cfce06a84c97b2f24642324989cd
aghvbc <barantaran2007@gmail.com>
2025-02-10 13:45:00 +0300

    First_task

---
+++ C_Practise_lesson_1/First_task.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int weight;
+
+    printf("Введите ваш вес:");
+    scanf("%d", &weight);
+
+    if (weight < 60)
+        printf("Вы не прошли отбор!");
+
+    else if (weight > 90)
+        printf("Вы не прошли отбор!");
+
+    else 
+        printf("Вы прошли отбор!");
+
+    return 0;
+}
