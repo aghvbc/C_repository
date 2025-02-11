de0e98468fa305bf160c755bb50f91fc92aa8d06
aghvbc <barantaran2007@gmail.com>
2025-02-11 20:35:57 +0300

    Third_task_added

---
+++ C_Practise_lesson_1/Third_task.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[100]; 
+
+    printf("Введите один символ (от A до Z): ");
+    scanf("%s", input); 
+
+    if (strlen(input) != 1) {
+        printf("Введите символ от A до Z.\n");
+    } else {
+        char character = input[0]; 
+        
+        if (character >= 'A' && character <= 'Z') {
+            printf("Символ '%c' входит в допустимый диапазон.\n", character);
+        } else {
+            printf("Символ '%c' не входит в допустимый диапазон.\n", character);
+        }
+    }
+
+    return 0;
+}
+
