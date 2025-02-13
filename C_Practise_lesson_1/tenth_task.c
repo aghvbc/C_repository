2ff173c068b5b5320ab7268b46bd1e4568bf3a73
aghvbc <barantaran2007@gmail.com>
2025-02-13 11:31:07 +0300

    All_tasks

--- C_Practise_lesson_1/tenth_task.c
+++ C_Practise_lesson_1/tenth_task.c
@@ -2,7 +2,15 @@
 #include <magic.h>
 
 int main(){
-    int T;
-    int N;
+    int t;
+    printf("Введите температуру корпуса: ");
+    scanf("%d", &t);
+
+    if (t>= -150 && t<= 120){
+        printf("Температура корпуса в норме");
+        return 0;
+    }
+
+    printf("Температура корпуса не в норме!");
     return 0;
 }
