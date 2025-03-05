79c97d699bfd42f9e097545be45eed39eae8f5d8
aghvbc <barantaran2007@gmail.com>
2025-03-05 21:34:59 +0300

    Task1_04_added

--- C_practise_4/04_problems/Task1.c
+++ C_practise_4/04_problems/Task1.c
@@ -20,19 +20,19 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void upper(char *str){
+void to_upper(char *str){
     for(int i = 0; str[i]; i++){
         str[i] = toupper(str[i]);
     }
 }
 
-void lower(char *str){
+void to_lower(char *str){
     for(int i = 0; str[i]; i++){
        str[i] = tolower(str[i]);
     }
 }
 
-void reverse(char *str) {
+void reverse_str(char *str) {
     int len = strlen(str);
     for (int i = 0; i < len / 2; i++) {
         char temp = str[i];
@@ -41,9 +41,10 @@ void reverse(char *str) {
     }
 }
 
-// void exit(){
-//     printf("Завершение программы");
-// }
+void exit_programm(){
+    printf("Завершение программы");
+    exit(0);
+}
 
 struct function{
     char *name;
@@ -52,20 +53,39 @@ struct function{
 
 int main() {
     char str[100] = "Hello, wrld!";
+    char str_r[100];
     struct function functions[] = 
     { 
-        {"upper", upper},
-        {"lower", lower},
-        {"reverse", reverse},
-        {"exit", exit}
+        {"upper", to_upper},
+        {"lower", to_lower},
+        {"reverse", reverse_str},
+        {"exit", exit_programm}
 
     };
-    while (not exit)
-    {
-        fgets();
-        printf("%s", str);
 
+    int n = sizeof(functions) / sizeof(functions[0]); 
+
+    while (1) {
+        printf("Введите команду (upper, lower, reverse, exit): ");
+        scanf("%s", str_r);
+
+        for (int i = 0; i < n; i++) {
+            if (strcmp(str_r, functions[i].name) == 0) {
+                if (strcmp(str_r, "exit") == 0) {
+                    functions[i].func(str); 
+                } else {
+                    functions[i].func(str);
+                    printf("Результат: %s\n", str);
+                }
+                break;
+            }
+        }
+
+    
+        if (strcmp(str_r, functions[n-1].name) != 0 && strcmp(str_r, functions[n-2].name) != 0 && strcmp(str_r, functions[n-3].name) != 0 && strcmp(str_r, functions[n-4].name) != 0) {
+            printf("Неизвестная команда: %s\n", str_r);
+        }
     }
-    // upper(str);
+
     return 0;
 }
