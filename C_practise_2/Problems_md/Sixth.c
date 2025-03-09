e4d3024514d1d504fb2c757d280a7cec7bdcfcad
aghvbc <barantaran2007@gmail.com>
2025-03-09 14:51:22 +0300

    update_sixth_from_pract2

--- C_practise_2/Problems_md/Sixth.c
+++ C_practise_2/Problems_md/Sixth.c
@@ -4,37 +4,67 @@
 
 #include <stdio.h>
 
-union Vehicle {
+typedef enum {
+    GASOLINE,
+    DIESEL,
+    ELECTRIC
+}EngineType;
+
+struct Vehicle {
     char mark[50];
     char model[50];
     int year_of_release;
-    char type_of_engine[50]; // в этой задаче я хотел видеть перечисление (по условию задачи речь идет об union)
+    EngineType enginetype;
 };
 
-int main() {
+int main() 
+{
     int choice;
     printf("Введите количество машин: ");
     scanf("%d", &choice);   
-    union Vehicle vehicle[choice];
+    struct Vehicle vehicle[choice];
 
-    for(int i = 0; i < choice; i++){
+    for(int i = 0; i < choice; i++)
+    {
         printf("Введите марку машины: ");
-        scanf("%s", &vehicle[i].mark);
+        scanf("%s", vehicle[i].mark);
         printf("Введите модель машины: ");
-        scanf("%s", &vehicle[i].model);
+        scanf("%s", vehicle[i].model);
         printf("Введите год выпуска: ");
         scanf("%d", &vehicle[i].year_of_release);
-        printf("Введите тип двигателя: ");
-        scanf("%s", &vehicle[i].type_of_engine);
+        printf("Выберите тип двигателя (0 - Gasoline, 1 - Diesel, 2 - Electric): ");
+        int engine_choice;
+        scanf("%d", &engine_choice);
+        if (engine_choice >= 0 && engine_choice <= 2) {
+            vehicle[i].enginetype = (EngineType)engine_choice;
+        } else {
+            printf("Ошибка: Неверный ввод типа двигателя. Устанавливаем Gasoline по умолчанию.\n");
+            vehicle[i].enginetype = GASOLINE;
+        }
+       
     }
     printf("\n");
-    for(int i = 0; i < choice; i++){
+    for(int i = 0; i < choice; i++)
+    {
         printf("Марка машины: %s %s\n", vehicle[i].mark, vehicle[i].model);
         printf("Год выпуска: %d\n",vehicle[i].year_of_release);
-        printf("Тип двигателя: %s\n",vehicle[i].type_of_engine);
+        printf("Тип двигателя: \n");
+        switch (vehicle[i].enginetype)
+        {
+            case GASOLINE: 
+                printf("Gasoline\n"); 
+                break;
+            case DIESEL: 
+                printf("Diesel\n"); 
+                break;
+            case ELECTRIC: 
+                printf("Electric\n"); 
+                break;
+            default:
+                printf("XZ\n");
+        }
         printf("\n");
     }
-
 
     return 0;
 }
